e8799d79d59ff755a3c4a1e8e629b3c4b6953fd4
Michael-Hanley <pasaquali@gmail.com>
2019-06-22 21:27:21 -0700

    Adds check to see if timer has been ran

--- src/app/shared/services/canDeactivatae-guard.service.ts
+++ src/app/shared/services/canDeactivatae-guard.service.ts
@@ -8,25 +8,29 @@ import { TimerComponent } from '../../meditation-timer/timer.component';
 import { ConfirmationDialogComponent } from '../components/confirmation-dialog/confirmation-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { map } from 'rxjs/operators';
+import { TimerService } from './timer.service';
 
 @Injectable()
 export class CanDeactivateGuard implements CanDeactivate<boolean> {
   dialogRef;
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, private timerService: TimerService) {}
 
-  }
-
-  canDeactivate(): Observable<boolean>| Promise<boolean> {
-    this.openDialog();
-    return this.dialogRef.afterClosed().pipe(
-      map(result => {
-        if (result) {
-          return true;
-        } else {
-          return false;
-        }
-      })
-    );
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if ( this.timerService.getStatus() ) {
+      this.openDialog();
+      return this.dialogRef.afterClosed().pipe(
+        map(result => {
+          if (result) {
+            this.timerService.resetTimer();
+            return true;
+          } else {
+            return false;
+          }
+        })
+      );
+    } else {
+      return true;
+    }
   }
   openDialog(): void {
     this.dialogRef = this.dialog.open(ConfirmationDialogComponent, {
